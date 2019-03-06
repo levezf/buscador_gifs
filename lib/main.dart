1a16dd8f9571cd724b1e4cfcee1c63e8419643e7
felipe <levezf>
2019-03-06 12:16:43 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:buscador_gifs/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'ui/home_page.dart';
+
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(hintColor: Colors.white),
+    ));
